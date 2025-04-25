cc7dd91523509c911a8f8bfaf829d0463112f450
ChiangRyan <jjs0613134571@gmail.com>
2025-04-25 17:05:02 +0800

    Reapply "加入專案檔案。"
    This reverts commit c181e85c0b13da8a22cf310c8c388458f32ec41a.

---
+++ SJAPP.Core/Helpers/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace SJAPP.Core.Helpers
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            System.Diagnostics.Debug.WriteLine($"OnPropertyChanged called for property: {propertyName}, Has subscribers: {PropertyChanged != null}");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Views/Home.xaml.cs
@@ -0,0 +1,36 @@
+﻿using SJAPP.Core.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SJAPP.Views
+{
+    public partial class Home : Page
+    {
+        private readonly HomeViewModel _viewModel;
+
+        public Home()
+        {
+            InitializeComponent();
+
+            var app = (App)Application.Current;
+            _viewModel = app.ServiceProvider.GetService<HomeViewModel>();
+            DataContext = _viewModel;
+
+            Loaded += Home_Loaded;
+            Unloaded += Home_Unloaded;
+        }
+
+        private void Home_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 當頁面載入時，啟動輪巡（如果需要）
+            _viewModel.StartPolling();
+            System.Diagnostics.Debug.WriteLine("Update timer started due to page load.");
+        }
+
+        private void Home_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _viewModel.StopPolling();
+        }
+    }
+}
