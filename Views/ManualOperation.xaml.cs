9ca4cd5fe25acea6bed0979663a92bd071f474db
ChiangRyan <jjs0613134571@gmail.com>
2025-04-25 17:26:11 +0800

    Reapply "加入專案檔案。"
    This reverts commit 4be86a49d84355c80ebcd40b3d0b087e53335e78.

---
+++ Views/ManualOperation.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SJAPP.Views
+{
+    /// <summary>
+    /// Monitor.xaml 的互動邏輯
+    /// </summary>
+    public partial class ManualOperation : Page
+    {
+        public ManualOperation()
+        {
+            InitializeComponent();
+        }
+    }
+}
